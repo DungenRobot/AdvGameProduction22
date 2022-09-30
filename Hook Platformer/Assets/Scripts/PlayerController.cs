ba7a60243b3ea31253a6ac8885147c84e64c4424
Charlotte <57119734+Charlotte-Stuff@users.noreply.github.com>
2022-09-30 11:02:35 -0400

    Grapple Moved Into Player Controller, Demo Scene Changed A Bit

--- Hook Platformer/Assets/Scripts/PlayerController.cs
+++ Hook Platformer/Assets/Scripts/PlayerController.cs
@@ -13,20 +13,29 @@ public class PlayerController : MonoBehaviour
     public float jump_velocity = 30f;
 
 
+    // Grapple Stuff
+    public float grappleStrength = 1;
+    public float maxGrappleLength = 10000;
+    public Transform[] grappleables;
+    private Transform currentGrappleTarget = null;
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
         controller = gameObject.GetComponent<CharacterController>();
+        grappleables = GetGrappleables();
     }
 
     // Update is called once per frame
     void Update()
     {
-        velocity.x = speed;
+        
 
         is_on_ground = controller.isGrounded;
+
+        if(is_on_ground) velocity.x = Mathf.Lerp(velocity.x, speed, Time.deltaTime * 0.5f);
 
         if (is_on_ground && Input.GetButton("Jump")){
             velocity.y = 5f;
@@ -36,7 +45,42 @@ public class PlayerController : MonoBehaviour
             velocity.y -= gravity * Time.deltaTime;
         }
 
-        controller.Move(velocity * Time.deltaTime);
+        velocity = velocity + Grapple();
+        controller.Move((velocity) * Time.deltaTime);
     }
 
+    Vector3 Grapple()
+    {
+        if(Input.GetKeyDown(KeyCode.Q)){
+            Debug.Log("Q Down");
+            float cd = Vector2.Distance(grappleables[0].position, this.transform.position);
+            Transform co = grappleables[0];
+
+            for(int i = 1; i<grappleables.Length;i++){
+                Transform grappleable = grappleables[i];
+                float dist = Vector2.Distance(grappleable.position, this.transform.position);
+                if(dist < cd){ co = grappleable; cd = dist;} 
+            }
+
+            if(cd <= maxGrappleLength) currentGrappleTarget = co;
+        }
+
+        if(Input.GetKeyUp(KeyCode.Q)) currentGrappleTarget = null;
+
+        if(currentGrappleTarget != null){
+
+            float angle = Mathf.Atan2(currentGrappleTarget.position.y - this.transform.position.y, currentGrappleTarget.position.x - this.transform.position.x);
+            return new Vector3(Mathf.Cos(angle), Mathf.Sin(angle),0) * grappleStrength;
+        }
+
+        return new Vector3(0,0,0);
+    }
+
+    Transform[] GetGrappleables(){
+        GameObject[] temp = GameObject.FindGameObjectsWithTag("grappleable");
+        List<Transform> temp2 = new List<Transform>();
+        foreach(GameObject go in temp)
+           temp2.Add(go.transform);
+        return temp2.ToArray();
+    }
 }
