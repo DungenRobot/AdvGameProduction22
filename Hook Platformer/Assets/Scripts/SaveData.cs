8858f7f84dffb8a6096dab2deb23deb3fdd7a90a
CherryGamer707 <cher011@gallowayschool.org>
2022-10-25 14:49:20 -0400

    Update that uploads process from save data
    Hello y'all, I realized that I cannot work further more on the save/data script without the game being completed with levels, functionality, and scoring. Right now, my objective is to work on the point system!!!

--- Hook Platformer/Assets/Scripts/SaveData.cs
+++ Hook Platformer/Assets/Scripts/SaveData.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
 
-public class SaveData : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+//public static class SaveData 
+//{
+    //Player is for referencing data from progress of the game
+   //public static void SavePlayer (Player player)
+   //{
+        //BinaryFormatter formatter = new BinaryFormatter();
+        //string path = Application.persistentDataPath + "/player.ballers";
+        //FileStream stream = new FileStream(path, FileMode.Create);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+        //PlayerData data = new PlayerData(player);
+
+        //formatter.Serialize(stream, data);
+        //stream.Close();
+   //}
+
+    //public static PlayerData LoadPlayer ()
+    //{
+        //string path = Application.persistentDataPath + "/player.ballers";
+        //if (File.Exists(path))
+        //{
+            //BinaryFormatter formatter = new BinaryFormatter();
+            //FileStream stream = new Filestream(path, FileMode.Open);
+
+            //PlayerData data = formatter.Deserialize(stream) as PlayerData;
+            //stream.Close();
+
+            //return data;
+        //}
+
+        //else
+        //{
+            //Debug.LogError("Save file not found in" + path);
+            //return null;
+        //}
+
+    //}
+
+//}
