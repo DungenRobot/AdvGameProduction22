09916eb6498cd31664d94f3ef1a1be9c56ba106c
DR4SLR <20050815x@gmail.com>
2022-11-14 15:38:47 -0500

    Parallax working now yay (kinda)
    don't look at the clouds

--- Hook Platformer/Assets/Scripts/Parallax.cs
+++ Hook Platformer/Assets/Scripts/Parallax.cs
@@ -4,23 +4,35 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-
-    private float length, startpos;
+    private float length, startpos, ypos;
     public GameObject cam;
-    public float ParallaxEffect;
+    public float parallaxEffect;
 
     // Start is called before the first frame update
     void Start()
     {
         startpos = transform.position.x;
+        ypos = transform.position.y;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        float dist = (cam.transform.position.x * ParallaxEffect);
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+        float ydist = (cam.transform.position.y * parallaxEffect);
 
-        transform.position =  new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        transform.position = new Vector3(startpos + dist, ypos + ydist, transform.position.z);
+        
+        if (temp > startpos + length)
+        {
+            Debug.Log("loop w length of " + length);
+            startpos += length;
+        }
+        else if (temp < startpos - length)
+        {
+            startpos -= length;
+        }
     }
 }
