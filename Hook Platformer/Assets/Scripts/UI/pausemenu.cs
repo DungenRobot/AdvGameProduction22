c5b52fd345b53c97294a4d5aee51b6857e6aee46
CherryGamer707 <cher011@gallowayschool.org>
2022-11-16 08:59:38 -0500

    Options Menu
    It functions, I'm pretty sure you can just copy and past the canvas into another scene with the gameplay.

--- Hook Platformer/Assets/Scripts/UI/pausemenu.cs
+++ Hook Platformer/Assets/Scripts/UI/pausemenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class pausemenu : MonoBehaviour
 {
     public GameObject pausemenuOBJ;
+    public GameObject settingmenuOBJ;
     public bool onpaused;
     // Start is called before the first frame update
     void Start()
@@ -37,13 +38,28 @@ public class pausemenu : MonoBehaviour
     {
         onpaused = true;
         pausemenuOBJ.SetActive(true);
+        settingmenuOBJ.SetActive(false);
     }
     //To resume game
     public void resume()
     {
         onpaused = false;
         pausemenuOBJ.SetActive(false);
+        settingmenuOBJ.SetActive(false);
 
+    }
+
+    //To Open the Setting Menu
+    public void settingopen()
+    {
+        settingmenuOBJ.SetActive(true);
+        pausemenuOBJ.SetActive(false);
+    }
+
+    public void settingclose()
+    {
+        settingmenuOBJ.SetActive(false);
+        pausemenuOBJ.SetActive(true);
     }
     //To Quit Game
     public void quit()
@@ -58,4 +74,31 @@ public class pausemenu : MonoBehaviour
         //for that pun
         SceneManager.LoadScene("Collision Basics");
     }
-}
+
+    public void mainmenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    //This is for the options menu
+
+    public void SetFullscreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void Resolution1920()
+    {
+        Screen.SetResolution(1920, 1080, true);
+    }
+
+    public void Resolution1280()
+    {
+        Screen.SetResolution( 1280, 800, true);
+    }
+
+    public void Resolution640()
+    {
+        Screen.SetResolution(640, 480, true);
+    }
+    }
