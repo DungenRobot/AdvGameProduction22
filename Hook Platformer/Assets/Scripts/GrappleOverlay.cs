704b8d2df81b3104edfdc4fc9fa16413622ba5ca
NAishere <104450078+NAishere@users.noreply.github.com>
2023-01-30 14:47:44 -0500

    Revert "Merge branch 'main' of https://github.com/DungenRobot/AdvGameProduction22"
    This reverts commit 98e0e31cb5a2ca46b1010ddb9001a83dcc3c8fcc, reversing
    changes made to 61f52301b7e709595fb6273d4aa46aea13ee1c92.

--- Hook Platformer/Assets/Scripts/GrappleOverlay.cs
+++ Hook Platformer/Assets/Scripts/GrappleOverlay.cs
@@ -1,13 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+
 public class GrappleOverlay : MonoBehaviour
 {
-    public Transform player;
     public SpriteRenderer sr;
-    public GameObject locationIndicator;
-
     Transform[] grappleables;
     public float maxGrappleLength;
     // Start is called before the first frame update
@@ -19,7 +16,6 @@ public class GrappleOverlay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
         float cd = Vector2.Distance(grappleables[0].position, this.transform.parent.position);
         int co = 0;
 
@@ -33,49 +29,6 @@ public class GrappleOverlay : MonoBehaviour
             sr.enabled = true;
             this.transform.position = new Vector3(grappleables[co].position.x, grappleables[co].position.y, 0);
         }else sr.enabled = false;
-
-        // Indicator
-
-        Vector2 pos = Camera.main.WorldToScreenPoint(transform.position);
- 
-        bool outOfBounds = !Screen.safeArea.Contains(pos);
-
-        float angleTo = Mathf.Atan2(grappleables[co].transform.position.y - player.position.y, grappleables[co].transform.position.x - player.position.x);
-        locationIndicator.GetComponent<RectTransform>().rotation = Quaternion.Euler(0,0, angleTo * Mathf.Rad2Deg - 45);
-        
-
-        if(outOfBounds){
-            locationIndicator.GetComponent<Image>().enabled = true; // Make Overlay Visable
-            // Draw Pointing At Edge Of The Screen
-            
-            Vector2 rotationRay = new Vector2(Mathf.Cos(angleTo), Mathf.Sin(angleTo));
-            Vector2 rectBounds = locationIndicator.transform.parent.GetComponent<RectTransform>().sizeDelta;
-            Vector2 newArrowPosition = new Vector2(0,0);
-        
-
-            if(Mathf.Abs(rectBounds.y/rotationRay.y) < Mathf.Abs(rectBounds.x/rotationRay.x)){
-                // Should be aligned on top or bottom
-                if(rotationRay.y > 0) newArrowPosition.y = rectBounds.y/2 - 50;
-                else newArrowPosition.y = 0 + 50;
-                newArrowPosition.x = rotationRay.x * rectBounds.y;
-            }else{
-                // Should be aligned on left or right
-                if(rotationRay.x > 0) newArrowPosition.x = rectBounds.x/2 - 50;
-                else newArrowPosition.x = -rectBounds.x/2 + 50;
-                newArrowPosition.y = rotationRay.y * (rectBounds.x/(2));
-                
-            }
-        locationIndicator.GetComponent<RectTransform>().localPosition = newArrowPosition;
-            
-            
-        }else{
-            // Stop Arrow From Drawing
-            locationIndicator.GetComponent<Image>().enabled = false;
-        }
-    }
-
-    void Indicator(){
-
     }
 
     void GetGrappleables(){
