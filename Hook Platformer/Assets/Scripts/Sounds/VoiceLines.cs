272fffba910dc20e05ddfa9399b370d4646384d1
Charlotte <57119734+Charlotte-Stuff@users.noreply.github.com>
2023-04-19 08:17:23 -0400

    Voice Lines

--- Hook Platformer/Assets/Scripts/Sounds/VoiceLines.cs
+++ Hook Platformer/Assets/Scripts/Sounds/VoiceLines.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class VoiceLines : MonoBehaviour
 {
+
+    public Line[] englishLines = {};
+    public Line[] spanishLines = {};
+
+    public float voiceLineDelay = 5; // Time between voice lines
+    public float voiceLineVariationTime = 1; // Variation in time between voice liens
+    private float voiceLineTimer = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,29 @@ public class VoiceLines : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        voiceLineTimer -= Time.deltaTime;
+        if(voiceLineTimer <= 0){
+            voiceLineTimer = voiceLineDelay + Random.Range(-voiceLineVariationTime, voiceLineVariationTime);
+            int voiceLine;
+            switch(LanguageManager.getInstance().language){
+                case "EN":
+                voiceLine = Random.Range(0,englishLines.Length-1);
+                englishLines[voiceLine].source.PlayOneShot(englishLines[voiceLine].clip, englishLines[voiceLine].volume);
+                break;
+                case "ES":
+                voiceLine = Random.Range(0,spanishLines.Length-1);
+                spanishLines[voiceLine].source.PlayOneShot(spanishLines[voiceLine].clip, spanishLines[voiceLine].volume);
+                break;
+            }
+            
+        }
+    }
+
+
+    [System.Serializable]
+    public class Line{
+        public AudioClip clip;
+        public AudioSource source;
+        public float volume;
     }
 }
